ad78d7575f8999d4ac015da50beefc7ee97f1c99
cadufelicioandrade <cadufelicioandrade@gmail.com>
2024-01-01 17:37:06 -0300

    Update-Project 2024-01-01v3

---
+++ PetStore.Web/Services/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using PetStore.Web.Models;
+
+namespace PetStore.Web.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> GetAllProducts(string token);
+        Task<ProductModel> GetProductById(string token, long id);
+        Task<ProductModel> CreatProduct(string token, ProductModel model);
+        Task<ProductModel> UpdateProduct(string token, ProductModel model);
+        Task<bool> DeleteProductById(string token, long id);
+    }
+}
---
+++ PetStore.Web/Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using PetStore.Web.Extensions;
+using PetStore.Web.Models;
+using PetStore.Web.Services.Interfaces;
+using System.Net.Http.Headers;
+
+namespace PetStore.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+        public const string BasePath = "api/product";
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<ProductModel>> GetAllProducts(string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> GetProductById(string token, long id)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<ProductModel> CreatProduct(string token, ProductModel model)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.PostAsJsonAsync(BasePath, model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProductModel>();
+            else throw new Exception("Something went wrong when calling API");
+        }
+        
+        public async Task<ProductModel> UpdateProduct(string token, ProductModel model)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.PutAsJsonAsync(BasePath, model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProductModel>();
+            else throw new Exception("Something went wrong when calling API");
+        }
+
+        public async Task<bool> DeleteProductById(string token, long id)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+            else throw new Exception("Something went wrong when calling API");
+        }
+    }
+}
